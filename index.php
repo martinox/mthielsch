3982fc4fd45cc7d49e5168eabfcf65c84a126949
Martin Thielsch <mt@mthielsch.de>
2016-03-29 15:27:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+	<meta name="description" content="">
+	<title>Martin Thielsch – Webdeveloper</title>
+	<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link rel='stylesheet' href='/asset/css/main.css' type='text/css' media='all' />
+</head>
+<body>
+	<header>
+		<h1>Hi, my name is <strong>Martin Thielsch</strong>, I'm a Webdeveloper from Germany.</h1>
+		<p>
+			I work as a backend developer (mainly PHP). I also do HTML, CSS, JS and Objective-c.
+		</p>
+		<p>
+
+		<a href="https://www.twitter.com/martiinox" target="_blank" class="social">
+		    <svg height="32" width="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4"></path> </svg>
+		</a>
+
+		<a href="https://www.github.com/martinox" target="_blank" class="social">
+			<img src="/asset/image/GitHub-Mark-120px-plus.png" width="32" height="32">
+		</a>
+
+		<a href="mailto:info@mthielsch.de" class="social">
+			<svg height="32" width="32" viewBox="0 0 20 20">
+				<path d="M17.388,4.751H2.613c-0.213,0-0.389,0.175-0.389,0.389v9.72c0,0.216,0.175,0.389,0.389,0.389h14.775c0.214,0,0.389-0.173,0.389-0.389v-9.72C17.776,4.926,17.602,4.751,17.388,4.751 M16.448,5.53L10,11.984L3.552,5.53H16.448zM3.002,6.081l3.921,3.925l-3.921,3.925V6.081z M3.56,14.471l3.914-3.916l2.253,2.253c0.153,0.153,0.395,0.153,0.548,0l2.253-2.253l3.913,3.916H3.56z M16.999,13.931l-3.921-3.925l3.921-3.925V13.931z"></path>
+			</svg>
+		</a>
+
+		<br>
+
+		<a href="/imprint">Imprint</a>
+
+		</p>
+	</header>
+</body>
+</html>
